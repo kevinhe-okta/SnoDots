f8c268badf95781efd23f516b89008e45cc39f74
kevinhe-okta <kevinhe-okta@users.noreply.github.com>
2015-06-01 15:11:09 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 sudo apt-get -y install git htop mailutils
 git clone https://github.com/kevinhe-okta/SnoDots.git
-cp -r ~/toybox/vim ~/.vim
-cp ~/toybox/vimrc ~/.vimrc
-cp ~/toybox/screenrc ~/.screenrc
-cat ~/toybox/bashrc > ~/.bashrc
+cp -r ~/Snodots/vim ~/.vim
+cp ~/Snodots/vimrc ~/.vimrc
+cp ~/Snodots/screenrc ~/.screenrc
+cat ~/Snodots/bashrc > ~/.bashrc
