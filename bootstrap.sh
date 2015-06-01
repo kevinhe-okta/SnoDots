349e4675501e7645cb2630cba6b9cd9f4587a362
kevinhe-okta <kevinhe-okta@users.noreply.github.com>
2015-06-01 16:26:54 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ cd ~
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
-sudo apt-get -y install git htop mailutils
+sudo apt-get -y install git htop tree
 git clone https://github.com/kevinhe-okta/SnoDots.git
 cp -r ~/SnoDots/vim ~/.vim
 cp ~/SnoDots/vimrc ~/.vimrc
